d8bda861789df54f7d2bbd3a8efcc604eda7e6d9
akshaykalyan12ka4 <55050637+akshaykalyan12ka4@users.noreply.github.com>
2020-04-09 18:18:47 +0530

    fibonacci by akshay kalyan

---
+++ fibonacci by akshay kalyan.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+n=int(input('enter a number upto which you want fibonacci sequence '))
+i=0
+j=1
+print(i)
+print(j)
+print(j)
+
+while i+j<=n: 
+    k=(i+j) 
+    i=j
+    j=k
+    
+    print(i+j)
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
